76c65dd45f673f98b177ae6f56351c4f09046b5f
e <2026750782@qq.com>
2020-03-05 12:47:16 +0800

    框架基本搭建

---
+++ src/cn/appsys/pojo/DevUser.java
@@ -0,0 +1,76 @@
+package cn.appsys.pojo;
+
+import java.util.Date;
+
+public class DevUser {
+     private Integer id;//开发者id
+	 public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getDevCode() {
+		return devCode;
+	}
+	public void setDevCode(String devCode) {
+		this.devCode = devCode;
+	}
+	public String getDevName() {
+		return devName;
+	}
+	public void setDevName(String devName) {
+		this.devName = devName;
+	}
+	public String getDevPassword() {
+		return devPassword;
+	}
+	public void setDevPassword(String devPassword) {
+		this.devPassword = devPassword;
+	}
+	public String getDevEmail() {
+		return devEmail;
+	}
+	public void setDevEmail(String devEmail) {
+		this.devEmail = devEmail;
+	}
+	public String getDevInfo() {
+		return devInfo;
+	}
+	public void setDevInfo(String devInfo) {
+		this.devInfo = devInfo;
+	}
+	public Integer getCreatedBy() {
+		return createdBy;
+	}
+	public void setCreatedBy(Integer createdBy) {
+		this.createdBy = createdBy;
+	}
+	public Date getCreationDate() {
+		return creationDate;
+	}
+	public void setCreationDate(Date creationDate) {
+		this.creationDate = creationDate;
+	}
+	public Integer getModifyBy() {
+		return modifyBy;
+	}
+	public void setModifyBy(Integer modifyBy) {
+		this.modifyBy = modifyBy;
+	}
+	public Date getModifyDate() {
+		return modifyDate;
+	}
+	public void setModifyDate(Date modifyDate) {
+		this.modifyDate = modifyDate;
+	}
+	 private String devCode;//开发者编码
+	 private String devName;//开发者姓名
+	 private String devPassword;//开发者密码
+	 private String devEmail;
+	 private String devInfo;
+	 private Integer createdBy;
+	 private Date creationDate;
+	 private Integer modifyBy;
+	 private Date modifyDate;
+}
---
+++ src/cn/appsys/pojo/Test.java
@@ -0,0 +1,13 @@
+package cn.appsys.pojo;
+
+public class Test {
+  private String test;
+
+public String getTest() {
+	return test;
+}
+
+public void setTest(String test) {
+	this.test = test;
+}
+}
